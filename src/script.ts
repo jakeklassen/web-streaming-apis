65d5c52811e9c26e552f5bdc9219ac3e4043b168
Jake Klassen <jklassendev@gmail.com>
2024-09-12 09:30:51 -0400

    chore: package upgrades and using temporal to query by date

--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,28 @@
 // Literally snagged from 🦕 https://deno.land/std@0.171.0/streams/text_line_stream.ts
-import { TextLineStream } from "./streams/text-line-stream.js";
+import { TextLineStream } from "./streams/text-line-stream.ts";
+import { Temporal } from "@js-temporal/polyfill";
+
+const now = Temporal.Now.plainDateISO();
+const targetDate = now
+  .subtract({ days: 1 })
+  .toLocaleString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  })
+  .split("/")
+  .join("_");
+
+const url = `https://files.tmdb.org/p/exports/movie_ids_${targetDate}.json.gz`;
+
+console.log("Fetching:", url);
 
 // Newline delimited JSON records
-const response = await fetch(
-  "https://files.tmdb.org/p/exports/movie_ids_07_01_2024.json.gz",
-);
+const response = await fetch(url, {
+  headers: {
+    "Accept-Encoding": "gzip",
+  },
+});
 
 const stream = response.body
   .pipeThrough(new DecompressionStream("gzip"))
